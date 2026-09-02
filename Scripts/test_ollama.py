a51f3b0081d020857cd4874ccb209efbb4cef4de
ezzyеzn <art.saemrui@gmail.com>
2026-09-02 13:59:58 +0200

    Update `test_ollama.py` to accept user input for chat messages

--- Scripts/test_ollama.py
+++ Scripts/test_ollama.py
@@ -1,9 +1,11 @@
 import ollama ##import the ollama module
 
+content = input("Enter your message: ") ## prompt the user to enter a message
+
 response = ollama.chat( ## call the chat function from the ollama module
     model="llama3.2:3b", ## name of the model we will use
     messages=[
         {"role": "user", ## role of the message sender, this role a standard role for user messages
-         "content": "Hello, how are you?"}, ## The message we send to the model
+         "content": content}, ## The message we send to the model
     ])
 print(response['message']['content']) ## print the response from the model
