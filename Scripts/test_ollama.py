c51da31b68668181931f5e38432e8ba9942b3cad
ezzyеzn <art.saemrui@gmail.com>
2026-09-02 13:42:07 +0200

    Refactor comments in `test_ollama.py` for clarity and consistency

--- Scripts/test_ollama.py
+++ Scripts/test_ollama.py
@@ -1,7 +1,7 @@
 import ollama ##import the ollama module
 
-response = ollama.chat(
-    model="llama3.2:3b", ## тame of the model we will use
+response = ollama.chat( ## call the chat function from the ollama module
+    model="llama3.2:3b", ## name of the model we will use
     messages=[
         {"role": "user", ## role of the message sender, this role a standard role for user messages
          "content": "Hello, how are you?"}, ## The message we send to the model
