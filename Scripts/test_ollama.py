25364c9f4a63638928a220a20e6facbad7fdb32a
ezzyеzn <art.saemrui@gmail.com>
2026-09-02 13:37:17 +0200

    Add initial implementation of `test_ollama.py` for model interaction

---
+++ Scripts/test_ollama.py
@@ -0,0 +1,9 @@
+import ollama ##import the ollama module
+
+response = ollama.chat(
+    model="llama3.2:3b", ## тame of the model we will use
+    messages=[
+        {"role": "user", ## role of the message sender, this role a standard role for user messages
+         "content": "Hello, how are you?"}, ## The message we send to the model
+    ])
+print(response['message']['content']) ## print the response from the model
