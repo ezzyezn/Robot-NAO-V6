d994bb96011788e39cd32f6c0abe9de221d7645f
ezzyеzn <art.saemrui@gmail.com>
2026-09-02 14:12:26 +0200

    Enhance system instructions in `test_ollama.py` for improved AI response accuracy

--- Scripts/test_ollama.py
+++ Scripts/test_ollama.py
@@ -5,7 +5,25 @@ content = input("Enter your message: ") ## prompt the user to enter a message
 response = ollama.chat( ## call the chat function from the ollama module
     model="llama3.2:3b", ## name of the model we will use
     messages=[
+        {   "role": "system", ## role of the message sender, a role for setting rules for the LLM
+            "content": """ 
+                            Jesteś asystentem AI Technikum TEB Edukacja.
+
+                            Odpowiadaj tylko na pytania dotyczące Technikum TEB Edukacja.
+
+                            Odpowiadaj WYŁĄCZNIE na podstawie informacji,
+                            które otrzymasz w wiadomości użytkownika.
+
+                            Jeżeli nie masz wystarczających informacji, odpowiedz:
+                            "Nie mam wystarczających informacji na ten temat."
+
+                            Nie wymyślaj informacji.
+                        """ ##Instructions for the model to follow when generating a response
+            
+            },
+        
         {"role": "user", ## role of the message sender, this role a standard role for user messages
-         "content": content}, ## The message we send to the model
+         "content": content ## The message we send to the model
+            }, 
     ])
 print(response['message']['content']) ## print the response from the model
