0d771ebc358f642878ac91a17d6f799e2442d466
ezzyеzn <art.saemrui@gmail.com>
2026-09-05 09:46:23 +0200

    Refactor cosine similarity function for improved structure and clarity

--- Scripts/test_similarity_scalar.py
+++ Scripts/test_similarity_scalar.py
@@ -1,34 +1,27 @@
 import math
 
+def cosine_similarity(a ,b):
+    dot_product = 0
+    sum_a = 0
+    sum_b = 0
+    
+    for i in range(len(a)):
+        dot_product += a[i] * b[i]
+        
+    for i in range(len(a)):
+        sum_a += a[i] ** 2
+    
+    for i in range(len(b)):
+        sum_b += b[i] ** 2
+        
+    length_a = math.sqrt(sum_a)
+    length_b = math.sqrt(sum_b)
+    
+    return dot_product / (length_a * length_b)
+
 a = [1, 1, 1]
 b = [1, 1, 1]
 
-dot_product = 0
+result = cosine_similarity(a, b)
 
-sum_a = 0
-
-sum_b = 0
-
-##dot product
-for i in range(len(a)):
-    dot_product += a[i] * b[i]
-    
-    
-## lenght a
-for i in range(len(a)):
-    sum_a += a[i] ** 2
-    
-length_a = math.sqrt(sum_a)
-
-## lenght b
-for i in range(len(b)):
-    sum_b += b[i] ** 2
-
-length_b = math.sqrt(sum_b)
-
-##similarity
-
-cosine_similarity = dot_product / (length_a * length_b)
-
-
-print(cosine_similarity)
+print(result)
