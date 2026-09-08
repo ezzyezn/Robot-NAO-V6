f42bf1cc0d8a5e068c3daf48046bd59be025c853
ezzyеzn <art.saemrui@gmail.com>
2026-09-08 10:29:39 +0200

    Enhance scraper functionality: implement cache update from multiple URLs and improve text splitting logic

--- Scripts/scraper.py
+++ Scripts/scraper.py
@@ -27,3 +27,23 @@ def update_cache(url, filename):
 def load_cache(filename):
     with open(filename, "r", encoding="utf-8") as file:
         return file.read()
+
+def update_cache_from_irls(urls,filename):
+    all_text = ""
+    for url in urls:
+            html = download_page(url)
+            text = extract_text(html)
+            
+            all_text += text + "\n\n"
+    
+    save_text(all_text, filename)
+    
+def split_text(text,chunk_size=500):
+    chunks = []
+    
+    for i in range(0, len(text), chunk_size):
+        chunk = text [i:i + chunk_size]
+        
+        chunks.append(chunk)
+        
+    return chunks
--- Scripts/test_scraper.py
+++ Scripts/test_scraper.py
@@ -1,9 +1,9 @@
-from scraper import update_cache, load_cache
+from scraper import load_cache, update_cache_from_irls
 import os
 
 cache_file = "Scripts/school_cache.txt"
 
-url = ["https://szkolasrednia.teb.pl/miasta/d/gdansk/kontakt/",
+urls = ["https://szkolasrednia.teb.pl/miasta/d/gdansk/kontakt/",
        "https://szkolasrednia.teb.pl/miasta/d/gdansk/nasza-szkola/"]
 
 update = input("Update cache? (y/n): ")
@@ -12,14 +12,14 @@ if os.path.exists(cache_file) and update != "y":
     
     print("Cache already exists")
     
-else:
-        
+else:  
+    
     print("Downloading page...")
-
-    update_cache(url, cache_file)
-
+    
+    update_cache_from_irls(urls, cache_file)
+    
     print("Page downloaded")
 
-text = load_cache(cache_file)
+all_text = load_cache(cache_file)
+print(all_text[:1000])
 
-print(text[:1000])
