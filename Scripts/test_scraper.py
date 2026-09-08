b44f05bc0d7ab38a686fb0548a9bbbbc0b02dff3
ezzyеzn <art.saemrui@gmail.com>
2026-09-08 13:41:37 +0200

    Refactor test_scraper.py for improved readability and consistent formatting

--- Scripts/test_scraper.py
+++ Scripts/test_scraper.py
@@ -1,30 +1,38 @@
-from scraper import load_cache, update_cache_from_urls, split_text
 import os
-from retrieval import create_embeddings, save_embeddings, load_embeddings, find_best_chunk
+
+from scraper import load_cache, update_cache_from_urls, split_text
+from retrieval import (create_embeddings,
+                       save_embeddings,
+                       load_embeddings,
+                       find_best_chunk)
+
 
 cache_file = "Scripts/school_cache.txt"
 embeddings_file = "Scripts/embeddings.json"
 
-urls = ["https://szkolasrednia.teb.pl/miasta/d/gdansk/kontakt/",
-       "https://szkolasrednia.teb.pl/miasta/d/gdansk/nasza-szkola/"]
+
+urls = [
+    "https://szkolasrednia.teb.pl/miasta/d/gdansk/kontakt/",
+    "https://szkolasrednia.teb.pl/miasta/d/gdansk/nasza-szkola/"
+    ]
+
 
 update = input("Update cache? (y/n): ")
 
+
 if os.path.exists(cache_file) and update != "y":
-    
     print("Cache already exists")
-    
 else:  
-    
     print("Downloading page...")
-    
     update_cache_from_urls(urls, cache_file)
-    
     print("Page downloaded")
+
 
 all_text = load_cache(cache_file)
 
+
 chunks = split_text(all_text)
+
 
 if os.path.exists(embeddings_file) and update != "y":
     embeddings = load_embeddings(embeddings_file)
@@ -32,14 +40,18 @@ else:
     embeddings = create_embeddings(chunks)
     save_embeddings(embeddings, embeddings_file)
     
+
 question = input("Ask a question: ")
+
 
 best_chunk, best_similarity = find_best_chunk(question,
                                               chunks,
                                               embeddings)
 
+
 print("BEST CHUNK: ")
 print(best_chunk)
 
-print("BEST SIMILARITY: ",best_similarity)
+
+print("BEST SIMILARITY:",best_similarity)
 
