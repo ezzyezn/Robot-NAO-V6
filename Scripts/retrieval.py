2d298c0d13c3ddfc87db18dc84a4dd4c846759e0
ezzyеzn <art.saemrui@gmail.com>
2026-09-08 11:16:20 +0200

    Refactor code structure for improved readability and maintainability

--- Scripts/retrieval.py
+++ Scripts/retrieval.py
@@ -1,41 +1,14 @@
 import ollama
-from similarity import cosine_similarity
+import json
 
-# Find the best matching information for the users question
-def find_best_info(question, lines):
+def create_embeddings(chunks):
+    response = ollama.embed(
+        model="qwen3-embedding:0.6b",
+        input=chunks
+    )
     
-    # Convert the users question into an embedding vector
-    question_embedding = ollama.embed(
-    model="qwen3-embedding:0.6b",
-    input=question
-)["embeddings"][0]
-    
-    # Variables for storing the best result
-    found_info = ""
-    best_similarity = 0
-    
-    # Check every line from the knowledge base
-    for line in lines:
-        
-        # Convert the current line into an embedding vector
-        line_embedding = ollama.embed(
-            model="qwen3-embedding:0.6b",
-            input=line
-        )["embeddings"][0]
-    
-    # Compare the question vector with the line vector
-        similarity = cosine_similarity(
-            question_embedding,
-            line_embedding
-            ) 
-    
-    # Show similarity for testing
-        print(line, similarity)
-        
-        # Save the line if it is more similar than previous results
-        if best_similarity < similarity:
-            best_similarity = similarity
-            found_info = line
-        
-    # Return the best information and its similarity score
-    return found_info, best_similarity
+    return response["embeddings"]
+
+def save_embeddings(embeddings, filename):
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(embeddings,file)
--- Scripts/test_scraper.py
+++ Scripts/test_scraper.py
@@ -1,7 +1,9 @@
-from scraper import load_cache, update_cache_from_irls
+from scraper import load_cache, update_cache_from_irls, split_text
 import os
+from retrieval import create_embeddings, save_embeddings
 
 cache_file = "Scripts/school_cache.txt"
+json_file = "Scripts/embeddings.json"
 
 urls = ["https://szkolasrednia.teb.pl/miasta/d/gdansk/kontakt/",
        "https://szkolasrednia.teb.pl/miasta/d/gdansk/nasza-szkola/"]
@@ -21,5 +23,13 @@ else:
     print("Page downloaded")
 
 all_text = load_cache(cache_file)
-print(all_text[:1000])
+
+chunks = split_text(all_text)
+
+embed = create_embeddings(chunks)
+
+save_embeddings = save_embeddings(embed,json_file )
+
+print("Chunks:", len(chunks))
+print("Embeddings: ", len(embed))
 
