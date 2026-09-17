04ea96d6cad167cdc42166eca2b564500cfe5905
ezzyеzn <art.saemrui@gmail.com>
2026-09-17 11:56:55 +0200

    Refactor answer handling in nao_bridge to improve retrieval logic and ensure proper JSON formatting

--- Scripts/nao_bridge.py
+++ Scripts/nao_bridge.py
@@ -1,9 +1,21 @@
-from queue import Queue
+from queue import Queue, Empty
+import json
 
-answer = Queue()
+answers = Queue()
 
-answer.put("Cześć")
+def get_next_answer():
+    try:
+        text = answers.get_nowait()
+    except Empty:
+        text = ""
+    
+    message = {"text": text}
+    data = json.dumps(message, ensure_ascii=False)
+    data = data.encode("utf-8")
+    
+    return data
 
-text = answer.get_nowait()
+answers.put("Czesc")
 
-print(text)
+print(get_next_answer())
+print(get_next_answer())
